c504afd23690bf0ac8dfea54e59c676fadceab00
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-24 03:50:35 +0530

    Implemented database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
+// connect db
+connectDB();
 
 const app = express();
 
