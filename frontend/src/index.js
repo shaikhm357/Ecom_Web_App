d6c1fa64ca32450494ca6e28e13b9d93e9dcab6b
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-24 20:00:03 +0530

    Implemented ReduxToolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/index.css";
@@ -13,6 +14,7 @@ import "./assets/styles/bootstrap.custom.css";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./screens/Home";
 import Product from "./screens/Product";
+import store from "./store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
