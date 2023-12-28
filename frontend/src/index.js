64306fd901f0475c2fd425b415fe26788ebd782d
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-28 21:48:58 +0530

    create cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import "./assets/styles/bootstrap.custom.css";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./screens/Home";
 import Product from "./screens/Product";
+import Cart from "./screens/Cart.js";
 import store from "./store.js";
 
 const router = createBrowserRouter(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<Home />} />
       <Route path='/products/:id' element={<Product />} />
+      <Route path='/cart' element={<Cart />} />
     </Route>
   )
 );
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -15,7 +15,7 @@ const cartSlice = createSlice({
 
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
-          x._d === existItem._id ? item : x
+          x._id === existItem._id ? item : x
         );
       } else {
         state.cartItems = [...state.cartItems, item];
