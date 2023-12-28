ec27756f694aa6523a1225b748a20d3181b3824b
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-28 16:27:28 +0530

    created utils folder and cleaned the slice code

---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,27 @@
+// helper function
+export const addDecimal = (num) => {
+  return Math.floor((num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  // calculate items price
+  state.itemsPrice = addDecimal(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+
+  // shippingprice
+  state.shippingPrice = addDecimal(state.itemsPrice > 100 ? 0 : 10);
+
+  // tax price (15%)
+  state.taxPrice = addDecimal(Number(0.15 * state.itemsPrice).toFixed(2));
+
+  // total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+
+  localStorage.setItem("cart", JSON.stringify(state));
+  return state;
+};
