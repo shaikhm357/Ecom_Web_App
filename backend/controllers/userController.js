b3b8cc3778542e49a5920f0180ca7238cd677878
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-29 12:10:46 +0530

    Implemented simple login only matching password and email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,20 @@ import User from "../models/userModel.js";
 // @route   Post /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("Auth user");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Register user
@@ -64,4 +77,14 @@ const updateUser = asyncHandler(async (req, res) => {
   res.send("update user admin");
 });
 
-export { authUser, updateUser, getUserById, deleteUser, getUsers, updateUserProfile,getUserProfile, logoutUser, registerUser };
+export {
+  authUser,
+  updateUser,
+  getUserById,
+  deleteUser,
+  getUsers,
+  updateUserProfile,
+  getUserProfile,
+  logoutUser,
+  registerUser
+};
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ connectDB();
 
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
